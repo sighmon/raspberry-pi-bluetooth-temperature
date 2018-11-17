c5a261d3e8845542b90291fadcc72c2a4d95c570
sighmon <simon.loffler@gmail.com>
2018-11-17 17:54:13 +1030

    Add Arno's original script and instructions to get it running on a Raspberry Pi Zero W.

--- README.md
+++ README.md
@@ -1,2 +1,124 @@
-# raspberry-pi-bluetooth-temperature
-Connect a Raspberry Pi Zero W to an Oregon Scientific BLE temperature station.
+# Raspberry Pi Zero W to Oregon Scientific IDTW211R
+
+Connect a Raspberry Pi Zero W to an Oregon Scientific BLE temperature station and read temperature values in Python.
+
+<img width="100%" src="IDTW211R-Oregon-Scientific-weather-station.jpg" />
+
+## Hardware
+
+* Raspberry Pi Zero W
+* Oregon Scientific IDTW211R weather station
+
+## Software
+
+A Raspberry Pi Zero W with Raspbian Stretch has all of the tools to scan for your weather station. To find it's BLE address:
+
+`$ sudo hcitool lescan`
+
+You should be able to see it in the list, something like:
+
+`<BLE ADDRESS> IDTW211R`
+
+### Retrieve temperature data from the command line
+
+Now that we know the address, we can connect to it:
+
+```bash
+$ sudo gatttool -b <BLE ADDRESS> -t random -I
+[<BLE ADDRESS>][LE]> connect
+Attempting to connect to <BLE ADDRESS>
+Connection successful
+[<BLE ADDRESS>][LE]> primary
+attr handle: 0x0001, end grp handle: 0x0007 uuid: 00001800-0000-1000-8000-00805f9b34fb
+attr handle: 0x0008, end grp handle: 0x0008 uuid: 00001801-0000-1000-8000-00805f9b34fb
+attr handle: 0x0009, end grp handle: 0x0021 uuid: 74e7fe00-c6a4-11e2-b7a9-0002a5d5c51b
+attr handle: 0x0022, end grp handle: 0x002e uuid: 0000180a-0000-1000-8000-00805f9b34fb
+attr handle: 0x002f, end grp handle: 0xffff uuid: 0000180f-0000-1000-8000-00805f9b34fb
+```
+
+To receive data from it, we enable indication by writting 0x0002 (0x0001 for notification) in Client Characteristic Configuration Descriptor (UUID: 0x2902) of each characteristic. Write shall be placed in little endian format so:
+
+```bash
+[<BLE ADDRESS>][LE]> char-write-req 0x000c 0200
+Characteristic value was written successfully
+[<BLE ADDRESS>][LE]> char-write-req 0x000f 0200
+Characteristic value was written successfully
+[<BLE ADDRESS>][LE]> char-write-req 0x0012 0200
+Characteristic value was written successfully
+[<BLE ADDRESS>][LE]> char-write-req 0x0015 0100
+Characteristic value was written successfully
+[<BLE ADDRESS>][LE]> char-write-req 0x0018 0200
+Characteristic value was written successfully
+[<BLE ADDRESS>][LE]> char-write-req 0x001b 0200
+Characteristic value was written successfully
+[<BLE ADDRESS>][LE]> char-write-req 0x001e 0200
+Characteristic value was written successfully
+[<BLE ADDRESS>][LE]> char-write-req 0x0021 0200
+Characteristic value was written successfully
+[<BLE ADDRESS>][LE]> char-write-req 0x0032 0100
+Characteristic value was written successfully
+Indication   handle = 0x000e value: 00 19 01 00 00 00 00 00 
+Indication   handle = 0x0017 value: 01 f9 00 1d 01 ff 7f ff 7f 7f 7f 7f 7f ff ff 7f 7f 7f 7f 7f 
+Indication   handle = 0x0017 value: 82 7f 7f 7f f9 00 e3 00 1d 01 b6 00 ff 7f ff 7f ff 7f ff 7f 
+Indication   handle = 0x0020 value: 01 ff 7f ff 7f ff 7f ff 7f 7f 7f 7f 7f ff ff 7f 7f 7f 7f 7f 
+Indication   handle = 0x0020 value: 82 7f 7f 7f ff 7f ff 7f ff 7f ff 7f ff 7f ff 7f ff 7f ff 7f 
+Indication   handle = 0x001d value: 11 12 0b 11 11 1e 34 ff 00 80 00 80 ff ff ff ff ff ff ff ff 
+[<BLE ADDRESS>][LE]> 
+```
+
+In return, you should start getting indications/notifications. The relevant ones for us are `INDOOR_AND_CH1_TO_3_TH_DATA` indications (so handle 0x0017).
+
+For each round of indications, we get two data packets of 20 bytes each. The most significant byte indicates the type of data (Type 0 or Type 1). See below for more details on the data packets.
+
+<img width="100%" src="INDOOR_AND_CH1_TO_3_TH_DATA_types.jpg" />
+
+### Retrieve temperature data via Python
+
+First install [bluepy](https://github.com/IanHarvey/bluepy):
+
+```bash
+$ sudo apt-get install python-pip libglib2.0-dev
+$ sudo pip install bluepy
+```
+
+Now run the python script handing in the address of your weather station:
+
+```bash
+$ python temperature.py <BLE ADDRESS>
+```
+
+If you'd rather get the script to scan for the weather station, you need to run it with root privileges:
+
+```bash
+$ sudo python temperature.py
+```
+
+You should see output:
+
+```bash
+$ python temperature.py <BLE ADDRESS>
+2018-11-17 17:41:38,600: WeatherStation connected !
+2018-11-17 17:41:38,603: Notifications enabled
+2018-11-17 17:41:38,939: handle e = 0019010000000000
+2018-11-17 17:41:39,135: indoorAndOutdoorTemp_type0 = 01fb002201ff7fff7f7f7f7f7fffff7f7f7f7f7f
+2018-11-17 17:41:39,330: indoorAndOutdoorTemp_type1 = 827f7f7ffb00e3002201b600ff7fff7fff7fff7f
+2018-11-17 17:41:39,524: handle 20 = 01ff7fff7fff7fff7f7f7f7f7fffff7f7f7f7f7f
+2018-11-17 17:41:39,671: handle 20 = 827f7f7fff7fff7fff7fff7fff7fff7fff7fff7f
+2018-11-17 17:41:39,817: handle 1d = 11120b11112927ff00800080ffffffffffffffff
+2018-11-17 17:41:40,819: Notification timeout
+2018-11-17 17:41:40,820: Indoor temp : 25.1°C, max : 25.1°C, min : 22.7°C
+2018-11-17 17:41:40,820: Outdoor temp : 29.0°C, max : 29.0°C, min : 18.2°C
+```
+### Run it hourly via a crontab
+
+To set it up to run hourly, edit your crontab:
+
+```bash
+$ crontab -e
+@hourly python /home/pi/temperature.py <BLE ADDRESS> >> /home/pi/cron-temperature.log
+```
+
+## TODO
+
+* Setup a [Flask](http://flask.pocoo.org) app to expose the temperature getting via an API - maybe use https://www.flaskapi.org
+* Connect that Flask app to Apple HomeKit using https://github.com/ikalchev/HAP-python
---
+++ temperature.py
@@ -0,0 +1,212 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Connect to Oregon Scientific BLE Weather Station
+# Copyright (c) 2016 Arnaud Balmelle
+#
+# This script will connect to Oregon Scientific BLE Weather Station
+# and retrieve the temperature of the base and sensors attached to it.
+# If no mac-address is passed as argument, it will scan for an Oregon Scientific BLE Weather Station.
+#
+# Supported Oregon Scientific Weather Station: EMR211 and RAR218HG (and probably BAR218HG)
+#
+# Usage: python bleWeatherStation.py [mac-address]
+#
+# Dependencies:
+# - Bluetooth 4.1 and bluez installed
+# - bluepy library (https://github.com/IanHarvey/bluepy)
+#
+# License: Released under an MIT license: http://opensource.org/licenses/MIT
+# Origin: https://www.instructables.com/id/Connect-Raspberry-Pi-to-Oregon-Scientific-BLE-Weat/
+
+import sys
+import logging
+import time
+import sqlite3
+from bluepy.btle import * 
+
+# uncomment the following line to get debug information
+logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.DEBUG)
+
+WEATHERSTATION_NAME = "IDTW211R" # IDTW213R for RAR218HG
+
+class WeatherStation:
+	def __init__(self, mac):
+		self._data = {}
+		try:
+			self.p = Peripheral(mac, ADDR_TYPE_RANDOM)
+			self.p.setDelegate(NotificationDelegate())
+			logging.debug('WeatherStation connected !')
+		except BTLEException:
+			self.p = 0
+			logging.debug('Connection to WeatherStation failed !')
+			raise
+			
+	def _enableNotification(self):
+		try:
+			# Enable all notification or indication
+			self.p.writeCharacteristic(0x000c, "\x02\x00")
+			self.p.writeCharacteristic(0x000f, "\x02\x00")
+			self.p.writeCharacteristic(0x0012, "\x02\x00")
+			self.p.writeCharacteristic(0x0015, "\x01\x00")
+			self.p.writeCharacteristic(0x0018, "\x02\x00")
+			self.p.writeCharacteristic(0x001b, "\x02\x00")
+			self.p.writeCharacteristic(0x001e, "\x02\x00")
+			self.p.writeCharacteristic(0x0021, "\x02\x00")
+			self.p.writeCharacteristic(0x0032, "\x01\x00")
+			logging.debug('Notifications enabled')
+		
+		except BTLEException as err:
+			print(err)
+			self.p.disconnect()
+	
+	def monitorWeatherStation(self):
+		try:
+			# Enable notification
+			self._enableNotification()
+			# Wait for notifications
+			while self.p.waitForNotifications(1.0):
+				# handleNotification() was called
+				continue
+			logging.debug('Notification timeout')
+		except:
+			return None
+		
+		regs = self.p.delegate.getData()
+		if regs is not None:
+			# expand INDOOR_AND_CH1_TO_3_TH_DATA_TYPE0
+			self._data['index0_temperature'] = ''.join(regs['data_type0'][4:6] + regs['data_type0'][2:4])
+			self._data['index1_temperature'] = ''.join(regs['data_type0'][8:10] + regs['data_type0'][6:8])
+			self._data['index2_temperature'] = ''.join(regs['data_type0'][12:14] + regs['data_type0'][10:12])
+			self._data['index3_temperature'] = ''.join(regs['data_type0'][16:18] + regs['data_type0'][14:16])
+			self._data['index0_humidity'] = regs['data_type0'][18:20]
+			self._data['index1_humidity'] = regs['data_type0'][20:22]
+			self._data['index2_humidity'] = regs['data_type0'][22:24]
+			self._data['index3_humidity'] = regs['data_type0'][24:26]
+			self._data['temperature_trend'] = regs['data_type0'][26:28]
+			self._data['humidity_trend'] = regs['data_type0'][28:30]
+			self._data['index0_humidity_max'] = regs['data_type0'][30:32]
+			self._data['index0_humidity_min'] = regs['data_type0'][32:34]
+			self._data['index1_humidity_max'] = regs['data_type0'][34:36]
+			self._data['index1_humidity_min'] = regs['data_type0'][36:38]
+			self._data['index2_humidity_max'] = regs['data_type0'][38:40]
+			# expand INDOOR_AND_CH1_TO_3_TH_DATA_TYPE1
+			self._data['index2_humidity_min'] = regs['data_type1'][2:4]
+			self._data['index3_humidity_max'] = regs['data_type1'][4:6]
+			self._data['index3_humidity_min'] = regs['data_type1'][6:8]
+			self._data['index0_temperature_max'] = ''.join(regs['data_type1'][10:12] + regs['data_type1'][8:10])
+			self._data['index0_temperature_min'] = ''.join(regs['data_type1'][14:16] + regs['data_type1'][12:14])
+			self._data['index1_temperature_max'] = ''.join(regs['data_type1'][18:20] + regs['data_type1'][16:18])
+			self._data['index1_temperature_min'] = ''.join(regs['data_type1'][22:24] + regs['data_type1'][20:22])
+			self._data['index2_temperature_max'] = ''.join(regs['data_type1'][26:28] + regs['data_type1'][24:26])
+			self._data['index2_temperature_min'] = ''.join(regs['data_type1'][30:32] + regs['data_type1'][28:30])
+			self._data['index3_temperature_max'] = ''.join(regs['data_type1'][34:36] + regs['data_type1'][32:34])
+			self._data['index3_temperature_min'] = ''.join(regs['data_type1'][38:40] + regs['data_type1'][36:38])
+			return True
+		else:
+			return None
+			
+	def getValue(self, indexstr):
+		val = int(self._data[indexstr], 16)
+		if val >= 0x8000:
+			val = ((val + 0x8000) & 0xFFFF) - 0x8000
+		return val
+	
+	def getIndoorTemp(self):
+		if 'index0_temperature' in self._data:
+			temp = self.getValue('index0_temperature') / 10.0
+			max = self.getValue('index0_temperature_max') / 10.0
+			min = self.getValue('index0_temperature_min') / 10.0
+			logging.debug('Indoor temp : %.1f°C, max : %.1f°C, min : %.1f°C', temp, max, min)
+			return temp
+		else:
+			return None
+	
+	def getOutdoorTemp(self):
+		if 'index1_temperature' in self._data:
+			temp = self.getValue('index1_temperature') / 10.0
+			max = self.getValue('index1_temperature_max') / 10.0
+			min = self.getValue('index1_temperature_min') / 10.0
+			logging.debug('Outdoor temp : %.1f°C, max : %.1f°C, min : %.1f°C', temp, max, min)
+			return temp
+		else:
+			return None
+			
+	def disconnect(self):
+		self.p.disconnect()
+		
+class NotificationDelegate(DefaultDelegate):
+	def __init__(self):
+		DefaultDelegate.__init__(self)
+		self._indoorAndOutdoorTemp_type0 = None
+		self._indoorAndOutdoorTemp_type1 = None
+		
+	def handleNotification(self, cHandle, data):
+		formatedData = binascii.b2a_hex(data)
+		if cHandle == 0x0017:
+			# indoorAndOutdoorTemp indication received
+			if formatedData[0] == '8':
+				# Type1 data packet received
+				self._indoorAndOutdoorTemp_type1 = formatedData
+				logging.debug('indoorAndOutdoorTemp_type1 = %s', formatedData)
+			else:
+				# Type0 data packet received
+				self._indoorAndOutdoorTemp_type0 = formatedData
+				logging.debug('indoorAndOutdoorTemp_type0 = %s', formatedData)
+		else:
+			# skip other indications/notifications
+			logging.debug('handle %x = %s', cHandle, formatedData)
+	
+	def getData(self):
+			if self._indoorAndOutdoorTemp_type0 is not None:
+				# return sensors data
+				return {'data_type0':self._indoorAndOutdoorTemp_type0, 'data_type1':self._indoorAndOutdoorTemp_type1}
+			else:
+				return None
+
+class ScanDelegate(DefaultDelegate):
+	def __init__(self):
+		DefaultDelegate.__init__(self)
+		
+	def handleDiscovery(self, dev, isNewDev, isNewData):
+		global weatherStationMacAddr
+		if dev.getValueText(9) == WEATHERSTATION_NAME:
+			# Weather Station in range, saving Mac address for future connection
+			logging.debug('WeatherStation found')
+			weatherStationMacAddr = dev.addr
+
+if __name__=="__main__":
+
+	weatherStationMacAddr = None
+	
+	if len(sys.argv) < 2:
+		# No MAC address passed as argument
+		try:
+			# Scanning to see if Weather Station in range
+			scanner = Scanner().withDelegate(ScanDelegate())
+			devices = scanner.scan(2.0)
+		except BTLEException as err:
+			print(err)
+			print('Scanning required root privilege, so do not forget to run the script with sudo.')
+	else:
+		# Weather Station MAC address passed as argument, will attempt to connect with this address
+		weatherStationMacAddr = sys.argv[1]
+	
+	if weatherStationMacAddr is None:
+		logging.debug('No WeatherStation in range !')
+	else:
+		try:
+			# Attempting to connect to device with MAC address "weatherStationMacAddr"
+			weatherStation = WeatherStation(weatherStationMacAddr)
+			
+			if weatherStation.monitorWeatherStation() is not None:
+				# WeatherStation data received
+				indoor = weatherStation.getIndoorTemp()
+				outdoor = weatherStation.getOutdoorTemp()
+			else:
+				logging.debug('No data received from WeatherStation')
+			
+			weatherStation.disconnect()
+		
+		except KeyboardInterrupt:
+			logging.debug('Program stopped by user')
+
